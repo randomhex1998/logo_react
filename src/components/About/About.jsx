2ba6b36f726e71f2051f364010ce09a31cb256a1
rayan <randomhex1998@gmail.com>
2022-12-13 16:33:43 +0330

    make-about-styles-in-desktop-and-mobile

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,16 +5,16 @@ const About = () => {
     return (
         <div className='about pTB-3'>
             <h3 className="MainTitles">آنچه ما انجام میدهیم</h3>
-            <div className="container">
+            <div className="container pt-2">
                 <div className="row">
-                    <AboutBOX AboutIco="fab fa-app-store-ios" AboutTitle="Without Title"/>
-                    <AboutBOX AboutIco="fas fa-drafting-compass" AboutTitle="Without Title"/>
-                    <AboutBOX AboutIco="fas fa-lightbulb" AboutTitle="Without Title"/>
+                    <AboutBOX AboutIco="fab fa-app-store-ios" AboutTitle="توسعه اپلیکیشن"/>
+                    <AboutBOX AboutIco="fas fa-drafting-compass" AboutTitle="طراحی گرافیک"/>
+                    <AboutBOX AboutIco="fas fa-lightbulb" AboutTitle="ایده خلاقانه"/>
                 </div>
                 <div className="row pt-2">
-                    <AboutBOX AboutIco="fas fa-ad" AboutTitle="Without Title"/>
-                    <AboutBOX AboutIco="fas fa-headset" AboutTitle="Without Title"/>
-                    <AboutBOX AboutIco="fas fa-copyright" AboutTitle="Without Title"/>
+                    <AboutBOX AboutIco="fas fa-ad" AboutTitle="مارکتینگ"/>
+                    <AboutBOX AboutIco="fas fa-headset" AboutTitle="پشتیبانی"/>
+                    <AboutBOX AboutIco="fas fa-copyright" AboutTitle="طراحی برند"/>
                 </div>
             </div>
         </div>
