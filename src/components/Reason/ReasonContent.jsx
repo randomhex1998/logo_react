2c7c28e095d2d4124bb0b6c4dfdeade3e4831dd0
rayan <randomhex1998@gmail.com>
2022-12-14 18:16:57 +0330

    add-reason-content-style

--- src/components/Reason/ReasonContent.jsx
+++ src/components/Reason/ReasonContent.jsx
@@ -2,7 +2,7 @@ const ReasonContent = () => {
     const ReasonIco = "fas fa-check-double";
     const ReasonOption = "این یک متن تست است";
    return(
-        <>
+        <div className="reason-content">
             <h3 className="MainTitles">دلیل انتخاب شرکت ما</h3>
             <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای</p>
             <ul>
@@ -10,7 +10,7 @@ const ReasonContent = () => {
                 <li><p>{ReasonOption}</p><i className={ReasonIco}></i></li>
                 <li><p>{ReasonOption}</p><i className={ReasonIco}></i></li>
             </ul>
-        </>
+        </div>
    )
 }
 
