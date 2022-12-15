c5d5c2482ae54ae61a1fd31e84d31cd31897b778
rayan <randomhex1998@gmail.com>
2022-12-16 01:04:48 +0330

    social-footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SocialFooter from './SocialFooter';
 
 const Footer = () => {
     return (
         <footer className='footer'>
             <div className="container">
                 <h2 className=''>LOGO</h2>
+                <SocialFooter/>
                 <p>کلیه حقوق برای <span>رایان جابری</span> محفوظ است</p>
             </div>
         </footer>
---
+++ src/components/Footer/SocialFooter.jsx
@@ -0,0 +1,23 @@
+const SocialFooter = () => {
+    return(
+    <div className="social-boxes">
+        <div className="social-area">
+            <i className="fa-brands fa-whatsapp"></i>
+        </div>
+        <div className="social-area">
+            <i className="fa-brands fa-telegram"></i>
+        </div>
+        <div className="social-area">
+            <i className="fa-brands fa-instagram"></i>
+        </div>
+        <div className="social-area">
+            <i className="fa-brands fa-twitter"></i>
+        </div>
+        <div className="social-area">
+            <i className="fa-brands fa-youtube"></i>
+        </div>
+    </div>
+    )
+}
+
+export default SocialFooter;
