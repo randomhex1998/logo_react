865a098f5f3e619267c7b744d804cc6292db3fd9
rayan <randomhex1998@gmail.com>
2022-12-15 16:10:29 +0330

    make-slider-responsive

--- src/components/Customers/CustomersSlide.jsx
+++ src/components/Customers/CustomersSlide.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import SlideBox from "./SlideBox";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
-
-// import "./styles.css";
 
 // import required modules
-import { Pagination, Navigation , Autoplay } from "swiper";
-// import SwiperCore, { Autoplay } from 'swiper';
+import {Autoplay } from "swiper";
 
 
 export default function CustSlide() {
@@ -31,9 +27,20 @@ export default function CustSlide() {
           delay: 3000,
           disableOnInteraction: false,
         }}
+        breakpoints={{
+          // when window width is >= 640px
+          1200: {
+            slidesPerView: 2,
+          },
+          // when window width is >= 768px
+          375: {
+            slidesPerView: 1,
+            slidesPerGroup: 1
+          },
+        }}
         
         navigation={true}
-        modules={[Pagination, Navigation , Autoplay]}
+        modules={[Autoplay]}
         className="mySwiper"
       >
         
